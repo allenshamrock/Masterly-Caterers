75b1d01e2a87367c3626848ee114215af05ecf57
Allen <allenshamrock37@gmail.com>
2024-09-02 18:10:08 +0300

    {Enh}:Configured the database url

--- server/config.py
+++ server/config.py
@@ -12,7 +12,7 @@ from flask_jwt_extended import JWTManager
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = '950f0f263d20fbb04b8a4b29a42c1377655a877c70df2ef1690d166bd5ff5198' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///masterly.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://masterly:yrXMBNoqOuylft9iSujRGkJHNeFyOaPk@dpg-crassqi3esus73ac3hgg-a/masterlydb'
 app.config['JWT_SECRET_KEY'] = 'f72c13cb465603659a3a537c55b4fd32946b66d6a4f3fa600121a9bdc79e9415'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 600
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,3 +27,4 @@ db.init_app(app)
 api = Api(app)
 CORS(app, supports_credentials=True)
 jwt=JWTManager(app)
+
